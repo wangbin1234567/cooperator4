bd769d867ba97f4ec8d2123ea8d82d14f519de4b
ffzl <634555688@qq.com>
2020-02-18 15:39:43 +0800

    路由传参实现

--- practical/pages/home/home.js
+++ practical/pages/home/home.js
@@ -8,6 +8,7 @@ Page({
     titleList: ['目标', '习惯','手记'],
     curIndex: 0,
     list: [{
+      title: '多彩周末体验计划',
       tain: [{
         flag: true,
         con: '看一场私人电影'
@@ -16,25 +17,41 @@ Page({
           con: '玩一次保龄球'
         }]
     },{
+        title: '我的习惯',
         tain: [{
+          fag: true,
           cun: 0,
           con: '每天早上空腹喝一杯水'
         }]
       },{
+        title: '我的手记',
         tain: [{
           con: '三月份找工作'
         }]
       }],
-     lists: []
+     lists: {},
+    str: [{
+      title: '目标',
+      lists: ['体验一次VR', '看完豆瓣电影top250']
+    }, {
+      title: '习惯',
+      lists: ['看书一小时', '看TED']
+    }]
   },
   bindViewTap: function () {
+    let drr=this.data.str.filter((item,index)=>item.title=='目标')
     wx.navigateTo({
-      url: '../target/target'
+      url: '../target/target',
+      success: function (res) {
+        // 通过eventChannel向被打开页面传送数据
+        let test = drr[0].lists
+        res.eventChannel.emit('acceptDataFromOpenerPage', test )
+      }
     })
   },
   handleIndex: function (event) {
     let index = event.currentTarget.dataset['index'];
-    let arr = this.data.list[index].tain
+    let arr = this.data.list[index]
     this.setData({
       curIndex: index,
       lists: arr
@@ -44,7 +61,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      
+    let arr = this.data.list[0]
+    this.setData({
+      lists: arr
+    })
   },
 
   /**
--- practical/pages/plaza/plaza.js
+++ practical/pages/plaza/plaza.js
@@ -1,25 +1,18 @@
-// pages/target/target.js
+// pages/plaza/plaza.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    lists: []
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const eventChannel = this.getOpenerEventChannel()
-    // 监听acceptDataFromOpenerPage事件，获取上一页面通过eventChannel传送到当前页面的数据
-    let dts=eventChannel.on('acceptDataFromOpenerPage', function (data) {
-        return data
-    })
-    this.setData({
-      lists: dts
-    })
+
   },
 
   /**
