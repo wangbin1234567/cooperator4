10c1695fe490c152aa8a835514c4792094085895
ffzl <634555688@qq.com>
2020-02-18 13:55:11 +0800

    增加tab切换

--- practical/pages/home/home.js
+++ practical/pages/home/home.js
@@ -5,14 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    titleList: ['目标', '习惯','手记'],
+    curIndex: 0,
+    list: [{
+      tain: [{
+        flag: true,
+        con: '看一场私人电影'
+      },{
+          flag: false,
+          con: '玩一次保龄球'
+        }]
+    },{
+        tain: [{
+          cun: 0,
+          con: '每天早上空腹喝一杯水'
+        }]
+      },{
+        tain: [{
+          con: '三月份找工作'
+        }]
+      }],
+     lists: []
   },
-
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../target/target'
+    })
+  },
+  handleIndex: function (event) {
+    let index = event.currentTarget.dataset['index'];
+    let arr = this.data.list[index].tain
+    this.setData({
+      curIndex: index,
+      lists: arr
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      
   },
 
   /**
